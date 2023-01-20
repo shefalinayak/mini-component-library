a3cc11a90b9e222dc51899f8f4268d92e03e9049
Shefali <nayakshefali@gmail.com>
2023-01-20 11:55:48 -0500

    select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,61 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <CustomSelect>
+        {displayedValue}
+        <StyledIcon id="chevron-down" size="24" strokeWidth="2" />
+      </CustomSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+`;
+
+const CustomSelect = styled.div`
+  font-family: 'Roboto', sans-serif;
+  font-size: 1rem;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+  padding: 12px 52px 12px 16px;
+
+  ${NativeSelect}:focus + & {
+    outline-offset: 2px;
+    outline: 1px dotted #212121;
+    outline: 5px auto Highlight;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const StyledIcon = styled(Icon)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  height: 24px;
+  margin: auto;
+  right: 10px;
+  pointer-events: none;
+`;
 
 export default Select;
