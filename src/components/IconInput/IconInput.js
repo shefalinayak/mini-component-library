1b279cb4a8aad371025f17a2e0e7c5525e63f61d
Shefali <nayakshefali@gmail.com>
2023-01-20 14:47:51 -0500

    icon input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,69 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const iconSize = size === 'small' ? 16 : 24; 
+  return (
+    <Wrapper size={size}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <TextInput type="text" placeholder={placeholder} width={width} style={styles[size]}/>
+      <IconWrapper style={{'--size': iconSize + 'px'}}>
+        <Icon id={icon} size={iconSize} strokeWidth="2"/>
+      </IconWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.label`
+  position: relative;
+  color: ${COLORS.gray700};
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const styles = {
+  small: {
+    '--borderWidth': '1px',
+    '--padding': '0 0 0 24px',
+    '--fontSize': 14/16 + 'rem',
+    '--height': 24/16 + 'rem',
+  },
+  large: {
+    '--borderWidth': '2px',
+    '--padding': '0 0 0 32px',
+    '--fontSize': 18/16 + 'rem',
+    '--height': 36/16 + 'rem',
+  },
+};
+
+const TextInput = styled.input`
+  border: none;
+  border-bottom: var(--borderWidth) solid ${COLORS.black};
+  padding: var(--padding);
+  height: var(--height);
+
+  font-family: 'Roboto', sans-serif;
+  font-size: var(--fontSize);
+  font-weight: 700;
+  color: inherit;
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+  
+  &:focus {
+    outline-offset: 2px;
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  height: var(--size);
+  margin: auto 0;
+`;
+
 
 export default IconInput;
